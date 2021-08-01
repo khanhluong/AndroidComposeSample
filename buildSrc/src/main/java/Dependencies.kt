babb9385c14756995ed0096168ee156126dbf4ac
khanh.luong <baokhanh2710cntt@gmail.com>
2021-08-01 21:44:34 +1200

    Migration to KTS.

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,13 @@
+const val kotlinVersion = "1.3.21"
+
+
+object BuildPlugins {
+    const val compileSdkVersion = 30
+
+}
+
+
+class Dependencies {
+
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "ComposeApp"
-include ':app'
+include(":app")
